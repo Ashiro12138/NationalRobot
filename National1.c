9f13f9c11517842bb57a846d66f7a0f4fc9c73c6
unknown <24618@Z20TB-24618.student.bbc.campus>
2016-09-08 16:06:03 +1000

    Removed some comments on National1

--- National1.c
+++ National1.c
@@ -98,21 +98,11 @@ task main()
 			switch(muxedSensor[i].typeMode)
 			{
 			case colorReflectedLight:
-				/*displayTextLine(i*3, "Chan[%d]: Ref Light", i+1);
-				displayTextLine(i*3 + 1, "Value: %d", muxedSensor[i].light);*/
 				mux[i] = muxedSensor[i].light;
 				break;
 			}
 		}
 		//Displaying data
-		/*displayTextLine(1, "Value: %d", mux[0]);
-		displayTextLine(4, "Value: %d", mux[1]);
-		displayTextLine(7, "Value: %d", mux[2]);
-		displayTextLine(10,"Inf1: %i",inf1);
-		displayTextLine(11,"Inf1s: %i",inf1s);
-		displayTextLine(12,"Inf2: %i",inf2);
-		displayTextLine(13,"Inf2s: %i",inf2s);
-		displayTextLine(14,"Comp: %i",comp);*/
 		//Orbit Logic
 		if(comp>=mincomp&&comp<=maxcomp){
 			Rcomp = true;
